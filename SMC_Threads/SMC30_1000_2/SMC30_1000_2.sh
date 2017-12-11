43609688d5edda3e24aca083d7626adafca97e6e
shijiaw <shijiaw@sfu.ca>
2017-12-10 20:27:48 -0800

    Add files via upload

---
+++ SMC_Threads/SMC30_1000_2/SMC30_1000_2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+#SBATCH -t 2-00:15
+#SBATCH --account=def-liang-ab
+#SBATCH --mem-per-cpu=4G      # memory; default unit is megabytes
+#SBATCH --cpus-per-task=2
+
+
+
+#echo "Using R:"
+module load r/3.4.0
+
+module load java/1.8.0_121
+
+#echo "Starting  nextflow at `date`."
+nextflow run  SMC30_1000_2.nf 
+#echo "Finished nextflow  at `date`."
+
